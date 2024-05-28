97d6f0d5d97cdf5f17b2766c134b021d0c743cf1
Sudipto Das <dassudipto200@gmail.com>
2024-05-28 14:09:58 +0530

    adding cookie

--- background.js
+++ background.js
@@ -6,7 +6,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         console.log('Tab updated');
         console.log(changeInfo);
         console.log(tab);
+
        if(tab.url.includes(url)){
+
+        //get cookie from the tab
+        chrome.cookies.getAll({url:'https://sudipto.eastus.cloudapp.azure.com:8080/api'}, function(cookies) {
+            console.log(cookies);
+        });
+
         chrome.scripting.executeScript({target: {tabId: tabId}, files:["content.js"]}).then(() => {
             chrome.tabs.sendMessage(tabId, {message: "updated"}, response => {
                 console.log(response);
